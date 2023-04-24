4beb882b310b23e7085db6a57d6247e149ec46d1
Guilherme <gui.wustro@gmail.com>
2023-04-24 07:49:39 -0300

    feat: add utilis and page orders

---
+++ app/dashboard/orders/page.tsx
@@ -0,0 +1,9 @@
+export default function Orders() {
+	return (
+		<main className="flex min-h-screen flex-col items-center px-10 lg:ps-52">
+			<div className="pt-4 flex justify-between w-full items-center max-w-7xl">
+				<h1>Em desenvolvimento...</h1>
+			</div>
+		</main>
+	);
+}
